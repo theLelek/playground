6714ce35d4d252260900093c7035d404f9bb3a1d
leon heschl <heschl.leon@gmail.com>
2026-07-14 01:23:21 +0200

    sync builder

---
+++ java/src/design_patterns/builder/Director.java
@@ -0,0 +1,5 @@
+package design_patterns.builder;
+
+public class Director {
+
+}
---
+++ java/src/design_patterns/builder/Main.java
@@ -0,0 +1,7 @@
+package design_patterns.builder;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
